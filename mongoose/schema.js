aae0d9b9f7ac8a42b068e426b6390ea99ba612c2
meSajied <sajiedshahyousuf@gmail.com>
2020-07-08 22:30:09 +0600

    added: mongoose operation....

---
+++ mongoose/schema.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const dishSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	description: {
+		type: String,
+		required: true
+	}
+},{
+	timestamps: true
+});
+
+const commentSchema = new Schema({
+	rating:  {
+		type: Number,
+		min: 1,
+		max: 5,
+		required: true
+	},
+	comment:  {
+		type: String,
+		required: true
+	},
+	author:  {
+		type: String,
+		required: true
+	}
+}, {
+	timestamps: true
+});
+
+var comment = mongoose.model('comment', commentSchema);
+var Dishes = mongoose.model('Dish', dishSchema);
+
+module.exports = Dishes;
