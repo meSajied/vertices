432cc8d56c94406e08924713f6a8d68b7c10b654
meSajied <sajiedshahyousuf@gmail.com>
2020-07-13 14:04:58 +0600

    added: cors process...

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// for cors...
+const cors = require('./cors');
 // for jsonwebtoken
 var authenticate = require('../authenticate');
 const dishRouter = express.Router();
@@ -133,3 +135,84 @@ dishRouter.route('/:dishId/comments/:commentId')
 	
 
 	});
+
+// for cors
+dishRouter.route('/')
+	.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+	.get(cors.cors, (req,res,next) => {
+	
+	})
+	
+	.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+	
+	})
+	
+	.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+	
+	
+	})
+	
+	.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+	
+	
+	})
+	
+	dishRouter.route('/:dishId')
+	.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+	.get(cors.cors, (req,res,next) => {
+	
+	})
+	
+	.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+	
+	})
+	
+	.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+	
+	
+	})
+	
+	.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+	
+	
+	})
+	
+	dishRouter.route('/:dishId/comments')
+	.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+	.get(cors.cors, (req,res,next) => {
+	
+	})
+	
+	.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+	
+	})
+	
+	.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+	
+	
+	})
+	
+	.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+	
+	
+	})
+	
+	dishRouter.route('/:dishId/comments/:commentId')
+	.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+	.get(cors.cors, (req,res,next) => {
+	
+	})
+	
+	.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+	
+	})
+	
+	.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+	
+	
+	})
+	
+	.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+	
+	
+	})
